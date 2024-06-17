84426f8d9f675275862f1b8f10a7159d89978e99
Koorosh Karkehabadi <koorosh.k98@gmail.com>
2024-06-18 01:22:00 +0330

    update main.js

--- functions/counter/src/main.js
+++ functions/counter/src/main.js
@@ -7,14 +7,25 @@ import { initializeApp } from "firebase-admin/app";
 const visitCounterCollection = 'VisitCounter';
 const usernameField = "Username";
 
-const apiKey = process.env.API_KEY;
-const projectId = process.env.PROJECT_ID;
-const appId = process.env.APP_ID;
 
-initializeApp({ apiKey: apiKey, projectId: projectId, appId: appId });
-console.log(apiKey);
-console.log(projectId);
-console.log(appId);
+
+const configs = {
+  type: process.env.type,
+  projectId: process.env.project_id,
+  privateKeyId: process.env.private_key_id,
+  privateKey: process.env.private_key,
+  clientEmail: process.env.client_email,
+  clientId: process.env.client_id,
+  authUri: process.env.auth_uri,
+  tokenUri: process.env.token_uri,
+  authProviderX509CertUrl: process.env.auth_provider_x509_cert_url,
+  clientC509CertUrl: process.env.client_x509_cert_url,
+  universe_domain: process.env.universe_domain
+}
+  
+
+initializeApp({credential: admin.credential.cert( configs )});
+console.log(configs);
 
 function randomInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
