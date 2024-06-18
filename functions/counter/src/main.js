36acd9c3926baa4f42de6a30966f79e4c8d45c9c
Koorosh Karkehabadi <koorosh.k98@gmail.com>
2024-06-18 17:10:11 +0330

    update main.js

--- functions/counter/src/main.js
+++ functions/counter/src/main.js
@@ -22,16 +22,17 @@ const configs = {
   clientC509CertUrl: process.env.client_x509_cert_url,
   universe_domain: process.env.universe_domain
 }
-  
 
-initializeApp({credential: admin.credential.cert( configs )});
+
+initializeApp({ credential: admin.credential.cert(configs) });
 
 function randomInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 const icons = [
-  '<path d="M8 12c-1.01 0-1.782-.504-2.267-.945a4.72 4.72 0 01-.564-.614 3.31 3.31 0 01-.212-.305.75.75 0 011.284-.775 3.214 3.214 0 00.5.584c.341.31.769.555 1.259.555.49 0 .918-.246 1.258-.555a3.214 3.214 0 00.5-.584.75.75 0 011.285.775l-.212.305c-.128.167-.317.39-.564.614C9.782 11.495 9.01 12 8 12zM5 6a1 1 0 011-1h.007a1 1 0 010 2H6a1 1 0 01-1-1zM10 5a1 1 0 100 2h.007a1 1 0 100-2H10z"></path> <path fill-rule="evenodd" d="M8 0a8 8 0 100 16A8 8 0 008 0zM1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0z" clip-rule="evenodd"></path>',
+
+  `<g fill="#000000"><path d="M8 12c-1.01 0-1.782-.504-2.267-.945a4.72 4.72 0 01-.564-.614 3.31 3.31 0 01-.212-.305.75.75 0 011.284-.775 3.214 3.214 0 00.5.584c.341.31.769.555 1.259.555.49 0 .918-.246 1.258-.555a3.214 3.214 0 00.5-.584.75.75 0 011.285.775l-.212.305c-.128.167-.317.39-.564.614C9.782 11.495 9.01 12 8 12zM5 6a1 1 0 011-1h.007a1 1 0 010 2H6a1 1 0 01-1-1zM10 5a1 1 0 100 2h.007a1 1 0 100-2H10z"/><path fill-rule="evenodd" d="M8 0a8 8 0 100 16A8 8 0 008 0zM1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0z" clip-rule="evenodd"/></g>`,
   
 ];
 
@@ -64,16 +65,16 @@ export default async ({ req, res }) => {
   var iconIndex = 0;
   var colorIndex = 0;
   if (snapshot != null && snapshot.docs != null && snapshot.docs.length > 0) {
-    snapshot.forEach(async doc => {    
-    const data = doc.data()
-    count = data["Count"];
-    label = data["Label"];
-    iconIndex = (data["IconIndex"] != null && data["IconIndex"] < icons.length) ? data["IconIndex"] : randomInteger(0, icons.length-1);
-    colorIndex = (data["ColorIndex"] != null && data["ColorIndex"] < colors.length) ? data["ColorIndex"] : randomInteger(0, colors.length-1);
+    snapshot.forEach(async doc => {
+      const data = doc.data()
+      count = data["Count"];
+      label = data["Label"];
+      iconIndex = (data["IconIndex"] != null && data["IconIndex"] < icons.length) ? data["IconIndex"] : randomInteger(0, icons.length - 1);
+      colorIndex = (data["ColorIndex"] != null && data["ColorIndex"] < colors.length) ? data["ColorIndex"] : randomInteger(0, colors.length - 1);
 
-    await doc.ref
-      .update({ "Count": ++count })
-      .timeout(2000);
+      await doc.ref
+        .update({ "Count": ++count })
+        .timeout(2000);
     });
   }
 
