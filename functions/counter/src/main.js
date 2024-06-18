91756b67527220aefdfe9c33d9d327a08eac49ff
Koorosh Karkehabadi <koorosh.k98@gmail.com>
2024-06-18 12:48:31 +0330

    update main.js

--- functions/counter/src/main.js
+++ functions/counter/src/main.js
@@ -160,8 +160,7 @@ export default async ({ req, res }) => {
 <div class="pill">
 <span class="pillLabel">
 <svg xmlns="http://www.w3.org/2000/svg" class="pillIcon heartbeat" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  // ${icons[iconIndex]}
-  <path name="default" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M352 64A64 64 0 1 0 224 64a64 64 0 1 0 128 0zM232.7 264l22.9 31.5c6.5 8.9 16.3 14.7 27.2 16.1s21.9-1.7 30.4-8.7l88-72c17.1-14 19.6-39.2 5.6-56.3s-39.2-19.6-56.3-5.6l-55.2 45.2-26.2-36C253.6 156.7 228.6 144 202 144c-30.9 0-59.2 17.1-73.6 44.4L79.8 280.9c-20.2 38.5-9.4 85.9 25.6 111.8L158.6 432H72c-22.1 0-40 17.9-40 40s17.9 40 40 40H280c17.3 0 32.6-11.1 38-27.5s-.3-34.4-14.2-44.7L187.7 354l45-90z" />
+  <path xmlns="http://www.w3.org/2000/svg" name="default" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"/>
 </svg>
 ${label}</span>
 <span class="pillCount"> ${count} </span>
